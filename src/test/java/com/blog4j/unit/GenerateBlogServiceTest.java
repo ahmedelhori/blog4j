8c383ad100dbf3442c069f277b05b738bbb62a8b
Ahmed Elhori <dev@elhori.com>
2021-06-13 16:00:27 +0200

    Fixed Tests

--- src/test/java/com/blog4j/unit/GenerateBlogServiceTest.java
+++ src/test/java/com/blog4j/unit/GenerateBlogServiceTest.java
@@ -1,5 +1,7 @@
 package com.blog4j.unit;
 
+import com.blog4j.entities.Post;
+import com.blog4j.entities.Tag;
 import com.blog4j.repositories.PostRepo;
 import com.blog4j.servicees.GenerateBlogService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,9 +13,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.thymeleaf.TemplateEngine;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -27,7 +35,18 @@ public class GenerateBlogServiceTest {
 
   @BeforeEach
   public void setup() {
+    Post post = new Post();
+    post.setVisible(false);
+    post.setTitle("Title title");
+    post.setAuthor("Test Author");
+    post.setCreateDate(LocalDate.now());
+    post.setEditDate(LocalDateTime.now());
+    post.setContent("Test content");
+    post.setTagsLine("test, PostTest");
     postRepo = mock(PostRepo.class);
+    List<Post> posts = new ArrayList<>();
+    posts.add(post);
+    when(postRepo.findByVisibleTrue()).thenReturn(posts);
     generator = new GenerateBlogService(postRepo, templateEngine, "test", "test", "test", tempDir.getPath(), "http://localhost:8080");
   }
 
